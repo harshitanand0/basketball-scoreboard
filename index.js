73fe372f2b578bf11c0ece246c102a942027d801
harshitanand0 <48909186+harshitanand0@users.noreply.github.com>
2022-09-08 04:44:55 -0700

    commit initial files

---
+++ index.js
@@ -0,0 +1,32 @@
+let button = document.getElementById("btn")
+let btn2 = document.getElementById("score")
+let score = 12
+
+function increment(){
+    score += 1 
+    btn2.textContent = score
+}
+function increment2(){
+    score += 2
+    btn2.textContent = score
+}
+function increment3(){
+    score+= 3
+    btn2.textContent = score
+}
+let btnn = document.getElementById("btn1")
+let btn3 = document.getElementById("score1")
+let score1 = 5
+
+function increment1(){
+    score1 += 1
+    btn3.textContent = score1
+}
+function incrementa(){
+    score1 += 2
+    btn3.textContent = score1 
+}
+function incrementb(){
+    score1 += 3
+    btn3.textContent = score1
+}
